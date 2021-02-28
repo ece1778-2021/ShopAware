63942c8e018174761bd4c669a700076acecc2e78
julyan keller-baruch <julyanbaruch@gmail.com>
2021-02-28 00:49:45 -0500

    second commit

---
+++ shopaware/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  shopaware
+//
+//  Created by Julyan Baruch on 2021-02-27.
+//
+
+import SwiftUI
+import Firebase
+import Foundation
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
+import SDWebImageSwiftUI
+
+struct ContentView: View {
+    @State var selection = 0
+        var body: some View {
+        ZStack{
+            VStack {
+                VStack(spacing: 4){
+                    HStack(spacing: 0){
+                        Text("Shop")
+                            .font(.system(size: 35, weight: .heavy))
+                            .foregroundColor(.purple)
+                        Text("Aware")
+                            .font(.system(size: 35, weight: .heavy))
+                            .foregroundColor(.orange)
+                    }
+                }.padding(.top)
+                
+                Picker("", selection: $selection) {
+                    Image(systemName: "cart.fill").tag(0)
+                    Image(systemName: "barcode.viewfinder").tag(1)
+                }.pickerStyle(SegmentedPickerStyle()).padding(.horizontal)
+                
+                if selection == 0 {
+                    Button(action: {}) {
+                        Text("Add a product")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.orange)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width-30)
+                            .background(Color.gray)
+                            .clipShape(Capsule())
+                    }.padding(.top, 22)
+                }
+                else {
+                    Button(action: {}) {
+                        Text("Scan a product")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.orange)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width-30)
+                            .background(Color.gray)
+                            .clipShape(Capsule())
+                    }.padding(.top, 22)
+                    
+                }
+            }
+        }
+    }
+}
---
+++ shopaware/shopawareApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopawareApp.swift
+//  shopaware
+//
+//  Created by Julyan Baruch on 2021-02-27.
+//
+
+import SwiftUI
+
+@main
+struct shopawareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
