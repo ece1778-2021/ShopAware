66b772122d2734d169f3b6eb7bc5d5a5d650524d
Salaar Liaqat <sliaqat@Salaars-MacBook-Pro.local>
2021-03-02 13:51:17 -0500

    fixed accident removal of files

--- shopaware/models/modelsAPI/EnforcementAPI.swift
+++ shopaware/models/modelsAPI/EnforcementAPI.swift
@@ -1,5 +1,5 @@
 //
-//  SuggestedActions.swift
+//  Enforcement.swift
 //  shopaware
 //
 //  Created by Salaar Liaqat on 2021-02-28.
@@ -7,30 +7,35 @@
 
 import Foundation
 
-struct SuggestedAction: Codable {
+struct EnforcementAPI: Codable {
     public var id: String
     public var assessment_id: String
     public var year: String
     public var country_id: String
     public var country: String
-    public var area: String
-    public var action: String
+    public var territory_id: String
+    public var territory_name: String
+    public var name: String
+    public var enforcement: String
     
     init(id: String,
          assessment_id: String,
          year: String,
          country_id: String,
          country: String,
-         area: String,
-         action: String) {
+         territory_id: String,
+         territory_name: String,
+         name: String,
+         enforcement: String) {
         self.id = id
         self.assessment_id = assessment_id
         self.year = year
         self.country_id = country_id
         self.country = country
-        self.area = area
-        self.action = action
-        
+        self.territory_id = territory_id
+        self.territory_name = territory_name
+        self.name = name
+        self.enforcement = enforcement
     }
     
 }
--- shopaware/models/modelsAPI/MechanismAPI.swift
+++ shopaware/models/modelsAPI/MechanismAPI.swift
@@ -1,5 +1,5 @@
 //
-//  Enforcement.swift
+//  Mechanism.swift
 //  shopaware
 //
 //  Created by Salaar Liaqat on 2021-02-28.
@@ -7,35 +7,33 @@
 
 import Foundation
 
-struct Enforcement: Codable {
+struct MechanismAPI: Codable {
     public var id: String
     public var assessment_id: String
     public var year: String
     public var country_id: String
     public var country: String
-    public var territory_id: String
-    public var territory_name: String
-    public var name: String
-    public var enforcement: String
+    public var coordination: String
+    public var program: String
+    public var policy: String
     
     init(id: String,
          assessment_id: String,
          year: String,
          country_id: String,
          country: String,
-         territory_id: String,
-         territory_name: String,
-         name: String,
+         coordination: String,
+         program: String,
+         policy: String,
          enforcement: String) {
         self.id = id
         self.assessment_id = assessment_id
         self.year = year
         self.country_id = country_id
         self.country = country
-        self.territory_id = territory_id
-        self.territory_name = territory_name
-        self.name = name
-        self.enforcement = enforcement
+        self.coordination = coordination
+        self.program = program
+        self.policy = policy
     }
     
 }
