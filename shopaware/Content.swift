f3d9233fbc3f7893cfaac148e1086fb3b68f2f68
Salaar Liaqat <sliaqat@Salaars-MacBook-Pro.local>
2021-03-21 15:03:56 -0400

    addede certification data to project

---
+++ shopaware/Content.swift
@@ -0,0 +1,21 @@
+//
+//  Content.swift
+//  shopaware
+//
+//  Created by Salaar Liaqat on 2021-03-21.
+//
+
+import Foundation
+
+
+class Content {
+    var title: String
+    var body: String
+    var link: String
+    
+    init(title: String, body: String, link: String) {
+        self.title = title
+        self.body = body
+        self.link =  link
+    }
+}
